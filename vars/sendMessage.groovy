b2f32fab6b356abc91264b4d0049636abc0a814e
Michal Srb <michal@redhat.com>
2020-03-24 13:23:34 +0100

    Add message templates

--- vars/sendMessage.groovy
+++ vars/sendMessage.groovy
@@ -20,10 +20,14 @@ def call(Map params = [:]) {
     // maybe we should consider having steps like "sendNotificationTestRunning()" (?)
     def taskId = artifactId.split(':')[1]
 
+    def msgTopic
     def msgTemplate
+
+    // TODO: running is very similar to queued - refactoring here...
     if (messageType == 'running') {
         def msgTemplateString = libraryResource 'koji-build.test.running-template.json'
         msgTemplate = new groovy.json.JsonSlurper().parseText(msgTemplateString)
+        msgTopic = 'org.centos.prod.ci.koji-build.test.running'
 
         // contact section
         msgTemplate['contact']['name'] = pipelineMetadata['pipelineName']
@@ -60,9 +64,154 @@ def call(Map params = [:]) {
         // misc
         msgTemplate['generated_at'] = Utils.getTimestamp()
         msgTemplate['version'] = msgVersion
-    } // TODO: else exception...
+    }
 
-    def msgTopic = 'org.centos.prod.ci.koji-build.test.running'
+    if (messageType == 'queued') {
+        def msgTemplateString = libraryResource 'koji-build.test.queued-template.json'
+        msgTemplate = new groovy.json.JsonSlurper().parseText(msgTemplateString)
+        msgTopic = 'org.centos.prod.ci.koji-build.test.queued'
+
+        // contact section
+        msgTemplate['contact']['name'] = pipelineMetadata['pipelineName']
+        msgTemplate['contact']['team'] = pipelineMetadata['maintainer']
+        msgTemplate['contact']['url'] = pipelineMetadata['docs']
+        msgTemplate['contact']['docs'] = pipelineMetadata['docs']
+        msgTemplate['contact']['irc'] = pipelineMetadata['contact']['irc']
+        msgTemplate['contact']['email'] = pipelineMetadata['contact']['email']
+
+        // run section
+        msgTemplate['run']['url'] = "${env.BUILD_URL}"
+        msgTemplate['run']['log'] = "${env.BUILD_URL}console"
+        msgTemplate['run']['log_raw'] = "${env.BUILD_URL}consoleText"
+        msgTemplate['run']['log_stream'] = "${env.BUILD_URL}console"
+
+        // artifact section
+        def koji = new Koji()
+        def taskInfo = koji.getTaskInfo(taskId.toInteger())
+        msgTemplate['artifact']['id'] = taskInfo.id
+        msgTemplate['artifact']['issuer'] = taskInfo.ownerName
+        msgTemplate['artifact']['component'] = taskInfo.name
+        msgTemplate['artifact']['nvr'] = taskInfo.nvr
+        msgTemplate['artifact']['scratch'] = taskInfo.scratch
+
+        // pipeline section
+        msgTemplate['pipeline']['id'] = Utils.generatePipelineId()
+        msgTemplate['pipeline']['id'] = pipelineMetadata['pipelineName']
+
+        // test section
+        msgTemplate['test']['type'] = pipelineMetadata['testType']
+        msgTemplate['test']['category'] = pipelineMetadata['testCategory']
+        msgTemplate['test']['namespace'] = 'fedora-ci.koji-build'
+
+        // misc
+        msgTemplate['generated_at'] = Utils.getTimestamp()
+        msgTemplate['version'] = msgVersion
+    }
+
+    if (messageType == 'complete') {
+        def msgTemplateString = libraryResource 'koji-build.test.complete-template.json'
+        msgTemplate = new groovy.json.JsonSlurper().parseText(msgTemplateString)
+        msgTopic = 'org.centos.prod.ci.koji-build.test.complete'
+
+        // contact section
+        msgTemplate['contact']['name'] = pipelineMetadata['pipelineName']
+        msgTemplate['contact']['team'] = pipelineMetadata['maintainer']
+        msgTemplate['contact']['url'] = pipelineMetadata['docs']
+        msgTemplate['contact']['docs'] = pipelineMetadata['docs']
+        msgTemplate['contact']['irc'] = pipelineMetadata['contact']['irc']
+        msgTemplate['contact']['email'] = pipelineMetadata['contact']['email']
+
+        // run section
+        msgTemplate['run']['url'] = "${env.BUILD_URL}"
+        msgTemplate['run']['log'] = "${env.BUILD_URL}console"
+        msgTemplate['run']['log_raw'] = "${env.BUILD_URL}consoleText"
+        msgTemplate['run']['log_stream'] = "${env.BUILD_URL}console"
+        msgTemplate['run']['debug'] = "${env.BUILD_URL}console"
+        msgTemplate['run']['rebuild'] = "${env.BUILD_URL}rebuild"
+
+        // artifact section
+        def koji = new Koji()
+        def taskInfo = koji.getTaskInfo(taskId.toInteger())
+        msgTemplate['artifact']['id'] = taskInfo.id
+        msgTemplate['artifact']['issuer'] = taskInfo.ownerName
+        msgTemplate['artifact']['component'] = taskInfo.name
+        msgTemplate['artifact']['nvr'] = taskInfo.nvr
+        msgTemplate['artifact']['scratch'] = taskInfo.scratch
+        msgTemplate['artifact']['baseline'] = taskInfo.nvr
+        msgTemplate['artifact']['source'] = taskInfo.source.raw
+
+        // pipeline section
+        msgTemplate['pipeline']['id'] = Utils.generatePipelineId()
+        msgTemplate['pipeline']['id'] = pipelineMetadata['pipelineName']
+
+        // test section
+        def result = 'needs_inspection'
+        if (currentBuild.result == 'SUCCESS') {
+            result = 'passed'
+        } else if (currentBuild.result == 'UNSTABLE') {
+            result = 'needs_inspection'
+        }
+
+        msgTemplate['test']['type'] = pipelineMetadata['testType']
+        msgTemplate['test']['category'] = pipelineMetadata['testCategory']
+        msgTemplate['test']['namespace'] = 'fedora-ci.koji-build'
+        msgTemplate['test']['note'] = ''
+        msgTemplate['test']['result'] = result
+        msgTemplate['test']['xunit'] = '' // TODO: how do we get this from TF?
+
+        // misc
+        msgTemplate['generated_at'] = Utils.getTimestamp()
+        msgTemplate['version'] = msgVersion
+    }
+
+    if (messageType == 'error') {
+        def msgTemplateString = libraryResource 'koji-build.test.error-template.json'
+        msgTemplate = new groovy.json.JsonSlurper().parseText(msgTemplateString)
+        msgTopic = 'org.centos.prod.ci.koji-build.test.error'
+
+        // contact section
+        msgTemplate['contact']['name'] = pipelineMetadata['pipelineName']
+        msgTemplate['contact']['team'] = pipelineMetadata['maintainer']
+        msgTemplate['contact']['url'] = pipelineMetadata['docs']
+        msgTemplate['contact']['docs'] = pipelineMetadata['docs']
+        msgTemplate['contact']['irc'] = pipelineMetadata['contact']['irc']
+        msgTemplate['contact']['email'] = pipelineMetadata['contact']['email']
+
+        // run section
+        msgTemplate['run']['url'] = "${env.BUILD_URL}"
+        msgTemplate['run']['log'] = "${env.BUILD_URL}console"
+        msgTemplate['run']['log_raw'] = "${env.BUILD_URL}consoleText"
+        msgTemplate['run']['log_stream'] = "${env.BUILD_URL}console"
+        msgTemplate['run']['debug'] = "${env.BUILD_URL}console"
+        msgTemplate['run']['rebuild'] = "${env.BUILD_URL}rebuild"
+
+        // artifact section
+        def koji = new Koji()
+        def taskInfo = koji.getTaskInfo(taskId.toInteger())
+        msgTemplate['artifact']['id'] = taskInfo.idreason
+        msgTemplate['artifact']['issuer'] = taskInfo.ownerName
+        msgTemplate['artifact']['component'] = taskInfo.name
+        msgTemplate['artifact']['nvr'] = taskInfo.nvr
+        msgTemplate['artifact']['scratch'] = taskInfo.scratch
+
+        // pipeline section
+        msgTemplate['pipeline']['id'] = Utils.generatePipelineId()
+        msgTemplate['pipeline']['id'] = pipelineMetadata['pipelineName']
+
+        // test section
+        msgTemplate['test']['type'] = pipelineMetadata['testType']
+        msgTemplate['test']['category'] = pipelineMetadata['testCategory']
+        msgTemplate['test']['namespace'] = 'fedora-ci.koji-build'
+        msgTemplate['test']['result'] = 'failed'
+
+        // test section
+        msgTemplate['error']['reason'] = 'Infrastructure Failure'
+        msgTemplate['error']['url'] = "${env.BUILD_URL}console"
+
+        // misc
+        msgTemplate['generated_at'] = Utils.getTimestamp()
+        msgTemplate['version'] = msgVersion
+    } // TODO: else exception...
 
     def msgProps = ''
     // def msgContent = JsonOutput.toJson(msgTemplate)
