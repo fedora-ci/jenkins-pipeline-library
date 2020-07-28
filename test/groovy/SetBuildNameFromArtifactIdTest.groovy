3289e6934240ab197c0bcdb0f28e028f5edad9fc
Michal Srb <michal@redhat.com>
2020-07-28 08:40:09 +0200

    Do not fail the pipeline if setBuildNameFromArtifactId step fails

---
+++ test/groovy/SetBuildNameFromArtifactIdTest.groovy
@@ -0,0 +1,36 @@
+import org.junit.*
+import com.lesfurets.jenkins.unit.*
+import static groovy.test.GroovyAssert.*
+
+
+class SetBuildNameFromArtifactIdTest extends BasePipelineTest {
+
+    @Before
+    void init() {
+        super.setUp()
+    }  
+
+    def setBuildNameFromArtifactId(params) {
+        super.setUp()
+        def step = helper.loadScript('vars/setBuildNameFromArtifactId.groovy', binding)
+        return step(params)
+    }
+
+    @Test
+    void testSetBuildNameFromArtifactId() {
+        def result = setBuildNameFromArtifactId(artifactId: 'koji-build:46436038')
+        assertEquals '[koji-build] wget-1.20.3-6.fc33', binding.getVariable('currentBuild').displayName.toString()
+    }
+
+    @Test
+    void testSetBuildNameFromArtifactIdBadArtifactType() {
+        def result = setBuildNameFromArtifactId(artifactId: 'unknown-build:46436038')
+        assertEquals "UNKNOWN ARTIFACT TYPE: 'unknown-build'", binding.getVariable('currentBuild').displayName.toString()
+    }
+
+    @Test
+    void testSetBuildNameFromArtifactIdError() {
+        def result = setBuildNameFromArtifactId(artifactId: 'koji-build:99999999')
+        assertEquals "INVALID ARTIFACT ID: 'koji-build:99999999'", binding.getVariable('currentBuild').displayName.toString()
+    }
+}
