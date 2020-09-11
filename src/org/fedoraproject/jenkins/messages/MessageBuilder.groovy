d30caab69fae5b812a053b630e6247270a68b349
Michal Srb <michal@redhat.com>
2020-09-11 22:04:45 +0200

    [sendMessage] Add support for pull requests

--- src/org/fedoraproject/jenkins/messages/MessageBuilder.groovy
+++ src/org/fedoraproject/jenkins/messages/MessageBuilder.groovy
@@ -6,6 +6,7 @@ import groovy.json.JsonSlurperClassic
 import org.fedoraproject.jenkins.koji.Koji
 import org.fedoraproject.jenkins.Utils
 import org.fedoraproject.jenkins.messages.RpmBuildMessageBuilder
+import org.fedoraproject.jenkins.messages.PullRequestMessageBuilder
 
 
 def getMessageVersion() {
@@ -18,6 +19,8 @@ def buildMessageQueued(String artifactType, String taskId, Map pipelineMetadata)
 
     if (artifactType == 'koji-build') {
         msg = new RpmBuildMessageBuilder().buildMessageQueued(artifactType, taskId, pipelineMetadata)
+    } else if (artifactType == 'fedora-dist-git') {
+        msg = new PullRequestMessageBuilder().buildMessageQueued(artifactType, taskId, pipelineMetadata)
     } else {
         throw new Exception("Unknown artifact type: ${artifactType}")
     }
@@ -33,6 +36,8 @@ def buildMessageRunning(String artifactType, String taskId, Map pipelineMetadata
 
     if (artifactType == 'koji-build') {
         msg = new RpmBuildMessageBuilder().buildMessageRunning(artifactType, taskId, pipelineMetadata)
+    } else if (artifactType == 'fedora-dist-git') {
+        msg = new PullRequestMessageBuilder().buildMessageRunning(artifactType, taskId, pipelineMetadata)
     } else {
         throw new Exception("Unknown artifact type: ${artifactType}")
     }
@@ -48,6 +53,8 @@ def buildMessageComplete(String artifactType, String taskId, Map pipelineMetadat
 
     if (artifactType == 'koji-build') {
         msg = new RpmBuildMessageBuilder().buildMessageComplete(artifactType, taskId, pipelineMetadata, xunit)
+    } else if (artifactType == 'fedora-dist-git') {
+        msg = new PullRequestMessageBuilder().buildMessageComplete(artifactType, taskId, pipelineMetadata, xunit)
     } else {
         throw new Exception("Unknown artifact type: ${artifactType}")
     }
@@ -63,6 +70,8 @@ def buildMessageError(String artifactType, String taskId, Map pipelineMetadata, 
 
     if (artifactType == 'koji-build') {
         msg = new RpmBuildMessageBuilder().buildMessageError(artifactType, taskId, pipelineMetadata, xunit)
+    } else if (artifactType == 'fedora-dist-git') {
+        msg = new PullRequestMessageBuilder().buildMessageError(artifactType, taskId, pipelineMetadata, xunit)
     } else {
         throw new Exception("Unknown artifact type: ${artifactType}")
     }
---
+++ src/org/fedoraproject/jenkins/messages/PullRequestMessageBuilder.groovy
@@ -0,0 +1,232 @@
+package org.fedoraproject.jenkins.messages
+
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurperClassic
+
+import org.fedoraproject.jenkins.pagure.Pagure
+import org.fedoraproject.jenkins.Utils
+
+
+def buildMessageQueued(String artifactType, String taskId, Map pipelineMetadata) {
+
+    def msgTemplate
+
+    def msgTemplateString = libraryResource 'pull-request.test.queued-template.json'
+    msgTemplate = new groovy.json.JsonSlurperClassic().parseText(msgTemplateString)
+
+    // contact section
+    msgTemplate['contact']['name'] = pipelineMetadata['pipelineName']
+    msgTemplate['contact']['team'] = pipelineMetadata['maintainer']
+    msgTemplate['contact']['url'] = pipelineMetadata['docs']
+    msgTemplate['contact']['docs'] = pipelineMetadata['docs']
+    msgTemplate['contact']['irc'] = pipelineMetadata['contact']['irc']
+    msgTemplate['contact']['email'] = pipelineMetadata['contact']['email']
+
+    // run section
+    msgTemplate['run']['url'] = "${env.BUILD_URL}"
+    msgTemplate['run']['log'] = "${env.BUILD_URL}console"
+    msgTemplate['run']['log_raw'] = "${env.BUILD_URL}consoleText"
+    msgTemplate['run']['log_stream'] = "${env.BUILD_URL}console"
+
+    // artifact section
+    def pagure = new Pagure()
+    def pullRequestInfo = pagure.getPullRequestInfo(taskId)
+    def uidCommitAndComment = pagure.splitPullRequestId(taskId)
+
+    msgTemplate['artifact']['type'] = 'fedora-dist-git'
+    msgTemplate['artifact']['repository'] = "${pagure.url}/${pullRequestInfo.get('project', [:])?.get('fullname')}"
+    msgTemplate['artifact']['pr_id'] = pullRequestInfo.get('id')
+    msgTemplate['artifact']['comment_id'] = uidCommitAndComment.get('commentId')
+    msgTemplate['artifact']['commit_hash'] = uidCommitAndComment.get('commitId')
+    msgTemplate['artifact']['issuer'] = pullRequestInfo.get('user', [:])?.get('name')
+    msgTemplate['artifact']['id'] = pullRequestInfo.get('uid')
+
+    // pipeline section
+    msgTemplate['pipeline']['id'] = Utils.generatePipelineId()
+    msgTemplate['pipeline']['name'] = pipelineMetadata['pipelineName']
+
+    // test section
+    msgTemplate['test']['type'] = pipelineMetadata['testType']
+    msgTemplate['test']['category'] = pipelineMetadata['testCategory']
+    msgTemplate['test']['namespace'] = "${pipelineMetadata['pipelineName'].replace(' ', '-').toLowerCase()}.${artifactType}"
+
+    // misc
+    msgTemplate['generated_at'] = Utils.getTimestamp()
+
+    return msgTemplate
+}
+
+
+def buildMessageRunning(String artifactType, String taskId, Map pipelineMetadata) {
+
+    def msgTemplate
+
+    def msgTemplateString = libraryResource 'pull-request.test.running-template.json'
+    msgTemplate = new groovy.json.JsonSlurperClassic().parseText(msgTemplateString)
+
+    // contact section
+    msgTemplate['contact']['name'] = pipelineMetadata['pipelineName']
+    msgTemplate['contact']['team'] = pipelineMetadata['maintainer']
+    msgTemplate['contact']['url'] = pipelineMetadata['docs']
+    msgTemplate['contact']['docs'] = pipelineMetadata['docs']
+    msgTemplate['contact']['irc'] = pipelineMetadata['contact']['irc']
+    msgTemplate['contact']['email'] = pipelineMetadata['contact']['email']
+
+    // run section
+    msgTemplate['run']['url'] = "${env.BUILD_URL}"
+    msgTemplate['run']['log'] = "${env.BUILD_URL}console"
+    msgTemplate['run']['log_raw'] = "${env.BUILD_URL}consoleText"
+    msgTemplate['run']['log_stream'] = "${env.BUILD_URL}console"
+
+    // artifact section
+    def pagure = new Pagure()
+    def pullRequestInfo = pagure.getPullRequestInfo(taskId)
+    def uidCommitAndComment = pagure.splitPullRequestId(taskId)
+
+    msgTemplate['artifact']['type'] = 'fedora-dist-git'
+    msgTemplate['artifact']['repository'] = "${pagure.url}/${pullRequestInfo.get('project', [:])?.get('fullname')}"
+    msgTemplate['artifact']['pr_id'] = pullRequestInfo.get('id')
+    msgTemplate['artifact']['comment_id'] = uidCommitAndComment.get('commentId')
+    msgTemplate['artifact']['commit_hash'] = uidCommitAndComment.get('commitId')
+    msgTemplate['artifact']['issuer'] = pullRequestInfo.get('user', [:])?.get('name')
+    msgTemplate['artifact']['id'] = pullRequestInfo.get('uid')
+
+    // pipeline section
+    msgTemplate['pipeline']['id'] = Utils.generatePipelineId()
+    msgTemplate['pipeline']['name'] = pipelineMetadata['pipelineName']
+
+    // test section
+    msgTemplate['test']['type'] = pipelineMetadata['testType']
+    msgTemplate['test']['category'] = pipelineMetadata['testCategory']
+    msgTemplate['test']['namespace'] = "${pipelineMetadata['pipelineName'].replace(' ', '-').toLowerCase()}.${artifactType}"
+
+    // misc
+    msgTemplate['generated_at'] = Utils.getTimestamp()
+
+    return msgTemplate
+}
+
+
+def buildMessageComplete(String artifactType, String taskId, Map pipelineMetadata, String xunit) {
+
+    def msgTemplate
+
+    def msgTemplateString = libraryResource 'pull-request.test.complete-template.json'
+    msgTemplate = new groovy.json.JsonSlurperClassic().parseText(msgTemplateString)
+
+    // contact section
+    msgTemplate['contact']['name'] = pipelineMetadata['pipelineName']
+    msgTemplate['contact']['team'] = pipelineMetadata['maintainer']
+    msgTemplate['contact']['url'] = pipelineMetadata['docs']
+    msgTemplate['contact']['docs'] = pipelineMetadata['docs']
+    msgTemplate['contact']['irc'] = pipelineMetadata['contact']['irc']
+    msgTemplate['contact']['email'] = pipelineMetadata['contact']['email']
+
+    // artifact section
+    def pagure = new Pagure()
+    def pullRequestInfo = pagure.getPullRequestInfo(taskId)
+    def uidCommitAndComment = pagure.splitPullRequestId(taskId)
+
+    msgTemplate['artifact']['type'] = 'fedora-dist-git'
+    msgTemplate['artifact']['repository'] = "${pagure.url}/${pullRequestInfo.get('project', [:])?.get('fullname')}"
+    msgTemplate['artifact']['pr_id'] = pullRequestInfo.get('id')
+    msgTemplate['artifact']['comment_id'] = uidCommitAndComment.get('commentId')
+    msgTemplate['artifact']['commit_hash'] = uidCommitAndComment.get('commitId')
+    msgTemplate['artifact']['issuer'] = pullRequestInfo.get('user', [:])?.get('name')
+    msgTemplate['artifact']['id'] = pullRequestInfo.get('uid')
+
+    // pipeline section
+    msgTemplate['pipeline']['id'] = Utils.generatePipelineId()
+    msgTemplate['pipeline']['id'] = pipelineMetadata['pipelineName']
+
+    // test section
+    def result = 'needs_inspection'
+    if (currentBuild.result == 'SUCCESS') {
+        result = 'passed'
+    } else if (currentBuild.result == 'UNSTABLE') {
+        result = 'needs_inspection'
+    }
+
+    msgTemplate['test']['type'] = pipelineMetadata['testType']
+    msgTemplate['test']['category'] = pipelineMetadata['testCategory']
+    msgTemplate['test']['namespace'] = "${pipelineMetadata['pipelineName'].replace(' ', '-').toLowerCase()}.${artifactType}"
+    msgTemplate['test']['note'] = ''
+    msgTemplate['test']['result'] = result
+    msgTemplate['test']['xunit'] = xunit
+
+    // run section
+    if (msgTemplate['test']['xunit']) {
+        msgTemplate['run']['url'] = "${env.BUILD_URL}testReport/(root)/tests/"
+    } else {
+        msgTemplate['run']['url'] = "${env.BUILD_URL}"
+    }
+    msgTemplate['run']['log'] = "${env.BUILD_URL}console"
+    msgTemplate['run']['log_raw'] = "${env.BUILD_URL}consoleText"
+    msgTemplate['run']['log_stream'] = "${env.BUILD_URL}console"
+    msgTemplate['run']['debug'] = "${env.BUILD_URL}console"
+    msgTemplate['run']['rebuild'] = "${env.BUILD_URL}rebuild"
+
+    msgTemplate['system'] = []  // do we need this?
+
+    // misc
+    msgTemplate['generated_at'] = Utils.getTimestamp()
+
+    return msgTemplate
+}
+
+
+def buildMessageError(String artifactType, String taskId, Map pipelineMetadata, String xunit) {
+
+    def msgTemplate
+
+    def msgTemplateString = libraryResource 'pull-request.test.error-template.json'
+    msgTemplate = new groovy.json.JsonSlurperClassic().parseText(msgTemplateString)
+
+    // contact section
+    msgTemplate['contact']['name'] = pipelineMetadata['pipelineName']
+    msgTemplate['contact']['team'] = pipelineMetadata['maintainer']
+    msgTemplate['contact']['url'] = pipelineMetadata['docs']
+    msgTemplate['contact']['docs'] = pipelineMetadata['docs']
+    msgTemplate['contact']['irc'] = pipelineMetadata['contact']['irc']
+    msgTemplate['contact']['email'] = pipelineMetadata['contact']['email']
+
+    // run section
+    msgTemplate['run']['url'] = "${env.BUILD_URL}"
+    msgTemplate['run']['log'] = "${env.BUILD_URL}console"
+    msgTemplate['run']['log_raw'] = "${env.BUILD_URL}consoleText"
+    msgTemplate['run']['log_stream'] = "${env.BUILD_URL}console"
+    msgTemplate['run']['debug'] = "${env.BUILD_URL}console"
+    msgTemplate['run']['rebuild'] = "${env.BUILD_URL}rebuild"
+
+    // artifact section
+    def pagure = new Pagure()
+    def pullRequestInfo = pagure.getPullRequestInfo(taskId)
+    def uidCommitAndComment = pagure.splitPullRequestId(taskId)
+
+    msgTemplate['artifact']['type'] = 'fedora-dist-git'
+    msgTemplate['artifact']['repository'] = "${pagure.url}/${pullRequestInfo.get('project', [:])?.get('fullname')}"
+    msgTemplate['artifact']['pr_id'] = pullRequestInfo.get('id')
+    msgTemplate['artifact']['comment_id'] = uidCommitAndComment.get('commentId')
+    msgTemplate['artifact']['commit_hash'] = uidCommitAndComment.get('commitId')
+    msgTemplate['artifact']['issuer'] = pullRequestInfo.get('user', [:])?.get('name')
+    msgTemplate['artifact']['id'] = pullRequestInfo.get('uid')
+
+    // pipeline section
+    msgTemplate['pipeline']['id'] = Utils.generatePipelineId()
+    msgTemplate['pipeline']['id'] = pipelineMetadata['pipelineName']
+
+    // test section
+    msgTemplate['test']['type'] = pipelineMetadata['testType']
+    msgTemplate['test']['category'] = pipelineMetadata['testCategory']
+    msgTemplate['test']['namespace'] = "${pipelineMetadata['pipelineName'].replace(' ', '-').toLowerCase()}.${artifactType}"
+    msgTemplate['test']['result'] = 'failed'
+
+    // test section
+    msgTemplate['error']['reason'] = 'Infrastructure Failure'
+    msgTemplate['error']['url'] = "${env.BUILD_URL}console"
+
+    // misc
+    msgTemplate['generated_at'] = Utils.getTimestamp()
+
+    return msgTemplate
+}
