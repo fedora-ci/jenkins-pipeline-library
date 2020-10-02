e7d7dbc7a843490fc12c117a48a20aba2d27915e
Michal Srb <michal@redhat.com>
2020-10-02 11:38:45 +0200

    [test] Test composite artifact in getIdFromArtifactId()

--- test/groovy/GetIdFromArtifactIdTest.groovy
+++ test/groovy/GetIdFromArtifactIdTest.groovy
@@ -8,7 +8,7 @@ class GetIdFromArtifactIdTest extends BasePipelineTest {
     @Before
     void init() {
         super.setUp()
-    }  
+    }
 
     def getIdFromArtifactId(params) {
         super.setUp()
@@ -26,5 +26,11 @@ class GetIdFromArtifactIdTest extends BasePipelineTest {
     void testGetIdFromAdditionalArtifactIds() {
         def result = getIdFromArtifactId(artifactId: 'koji-build:46436038', additionalArtifactIds: 'koji-build:123456789,koji-build:00000000')
         assertEquals '46436038,123456789,00000000', result
+    }
+
+    @Test
+    void testCompositeArtifact() {
+        def result = getIdFromArtifactId(artifactId: '(koji-build:46436038,koji-build:46436038)->fedora-update:FEDORA-2020-008cb761a2')
+        assertEquals '46436038,46436038', result
     }
 }
