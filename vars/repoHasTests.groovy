802164b95757ea61828c52118e41e675282859fa
Michal Srb <michal@redhat.com>
2020-10-08 11:25:20 +0200

    Add experimental repoHasTests() step

---
+++ vars/repoHasTests.groovy
@@ -0,0 +1,41 @@
+#!/usr/bin/groovy
+
+
+/**
+ * repoHasTests() step.
+ */
+def call(Map params = [:]) {
+
+    def repoUrl = params.get('repoUrl')
+    def ref = params.get('ref')
+    def stiUrl
+    def tmtUrl
+
+    // TODO: these templates should probably go to some config file
+    if (repoUrl.contains('src.fedoraproject.org')) {
+        stiUrl = repoUrl + "/blob/${ref}/f/tests/tests.yml"
+        tmtUrl = repoUrl + "/blob/${ref}/f/.fmf/version"
+    } else {
+        error("Unsupported repo URL: ${repoUrl}")
+    }
+
+    // check TMT
+    def response = httpRequest consoleLogResponseBody: false, httpMode: 'GET', url: "${tmtUrl}", validResponseCodes: '200,404'
+    if (response.status == 200) {
+        return 'tmt'
+    } else if (response.status == 404) {
+        // No TMT, but let's check STI
+    } else {
+        error("HTTP GET on ${tmtUrl} returned an unexpected return code: ${response.status}")
+    }
+
+    // check STI
+    response = httpRequest consoleLogResponseBody: false, httpMode: 'GET', url: "${stiUrl}", validResponseCodes: '200,404'
+    if (response.status == 200) {
+        return 'sti'
+    } else if (response.status == 404) {
+        // do nothing
+    } else {
+        error("HTTP GET on ${stiUrl} returned an unexpected return code: ${response.status}")
+    }
+}
