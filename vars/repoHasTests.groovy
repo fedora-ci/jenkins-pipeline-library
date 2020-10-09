c5414ddfc49d5ac75d839c0684ca82bbd2c3456f
Michal Srb <michal@redhat.com>
2020-10-09 13:50:34 +0200

    [repoHasTests] Clone repo to reliably find STI tests

--- vars/repoHasTests.groovy
+++ vars/repoHasTests.groovy
@@ -8,43 +8,29 @@ def call(Map params = [:]) {
 
     def repoUrl = params.get('repoUrl')
     def ref = params.get('ref')
-    def stiUrl
-    def tmtUrl
 
-    def pagureFedora = 'src.fedoraproject.org'
+    dir("temp-repoHasTests${env.BUILD_ID}") {
+        checkout([$class: 'GitSCM', branches: [[name: ref ]], userRemoteConfigs: [[url: repoUrl ]]])
 
-    // TODO: these templates should probably go to some config file
-    if (repoUrl.contains(pagureFedora)) {
-        // this is ugly:
-        // Pagure uses "https://<url>/forks/<user>/<ns>/<repo>" for cloning repositories,
-        // but "https://<url>/fork/..." (not "forks") when accessing repositories via web browser
-        //
-        // so we just "fix" the URL here
-        repoUrl = repoUrl.replace("${pagureFedora}/forks/", "${pagureFedora}/fork/")
+        sh('ls -la')
+        // check STI first
+        def stdStiFiles = findFiles glob: 'tests/tests*.yml'
+        def nonStdStiFiles = findFiles glob: 'tests*.yml'
 
-        stiUrl = repoUrl + "/blob/${ref}/f/tests/tests.yml"
-        tmtUrl = repoUrl + "/blob/${ref}/f/.fmf/version"
-    } else {
-        error("Unsupported repo URL: ${repoUrl}")
-    }
+        echo "STI tests in ${repoUrl} (${ref}): ${stdStiFiles} ${nonStdStiFiles}"
+        if (stdStiFiles || nonStdStiFiles) {
+            return 'sti'
+        }
 
-    // check FMF
-    def response = httpRequest consoleLogResponseBody: false, httpMode: 'GET', url: "${tmtUrl}", validResponseCodes: '200,404'
-    if (response.status == 200) {
-        return 'fmf'
-    } else if (response.status == 404) {
-        // No FMF, but let's check STI
-    } else {
-        error("HTTP GET on ${tmtUrl} returned an unexpected return code: ${response.status}")
-    }
+        // if STI tests were not found, let's try FMF
+        def stdFmf = findFiles glob: 'tests/tests*.yml'
+        echo "FMF tests in ${repoUrl} (${ref}): ${stdFmf}"
 
-    // check STI
-    response = httpRequest consoleLogResponseBody: false, httpMode: 'GET', url: "${stiUrl}", validResponseCodes: '200,404'
-    if (response.status == 200) {
-        return 'sti'
-    } else if (response.status == 404) {
-        // do nothing
-    } else {
-        error("HTTP GET on ${stiUrl} returned an unexpected return code: ${response.status}")
+        if (stdFmf) {
+            return 'fmf'
+        }
+
+        deleteDir()
+        return
     }
 }
