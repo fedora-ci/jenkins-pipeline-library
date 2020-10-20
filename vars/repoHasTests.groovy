0ac1e9cae5b7755220b0280e28b8fe3ffd8fe4c4
Michal Srb <michal@redhat.com>
2020-10-20 13:13:43 +0200

    [repoHasTests] Return only file paths

--- vars/repoHasTests.groovy
+++ vars/repoHasTests.groovy
@@ -18,7 +18,7 @@ def call(Map params = [:]) {
 
         echo "STI tests in ${repoUrl} (${ref}): ${stdStiFiles} ${nonStdStiFiles}"
         if (stdStiFiles || nonStdStiFiles) {
-            return [type: 'sti', files: stdStiFiles + nonStdStiFiles]
+            return [type: 'sti', files: (stdStiFiles + nonStdStiFiles).collect{ it.path } ]
         }
 
         // if STI tests were not found, let's try FMF
@@ -26,7 +26,7 @@ def call(Map params = [:]) {
         echo "FMF tests in ${repoUrl} (${ref}): ${stdFmf}"
 
         if (stdFmf) {
-            return [type: 'fmf', files: stdFmf]
+            return [type: 'fmf', files: stdFmf.collect{ it.path }]
         }
 
         deleteDir()
