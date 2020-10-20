f60deb615e67764c4f4d0a4808d022d980921d60
Michal Srb <michal@redhat.com>
2020-10-20 13:26:44 +0200

    [getRepoUrlAndRefFromTaskId] Print nice error message for packages built from custom SRPMs
    "fedpkg build --srpm <custom-srpm>" builds are not supported as there is
    not enough information in Koji about repo URLs and commit hashes.

--- vars/getRepoUrlAndRefFromTaskId.groovy
+++ vars/getRepoUrlAndRefFromTaskId.groovy
@@ -16,6 +16,11 @@ def call(taskId) {
 
         // srpmName is something like "fedora-ci_c7e32f545a8b4a0aa209b233c44b1f50_7579642717a0a4f83488560d50e2b5b5d76eaced_0;python-pygments-pytest.f34.src.rpm"
         def srpmName = buildInfo.source.raw.split('/')[-1]
+
+        if (!srpmName.startsWith('fedora-ci_')) {
+            error('Unable to determine the repo URL and the commit hash for packages built from custom SRPMs.')
+        }
+
         def prParts = srpmName.split(';')[0].split('_')
         // we want just the "python-pygments-pytest" part from the srpmName
         def repoName = srpmName.split(';')[1].split('.src.rpm')[0].split('\\.')[0].replace(':', '/')
