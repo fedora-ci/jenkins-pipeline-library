dc6e045223681825581ba39e72e3532ce27d1145
Michal Srb <michal@redhat.com>
2020-11-20 11:07:56 +0100

    [test] Test RH artifact in GetIdFromArtifactId() step

--- test/groovy/GetIdFromArtifactIdTest.groovy
+++ test/groovy/GetIdFromArtifactIdTest.groovy
@@ -33,4 +33,10 @@ class GetIdFromArtifactIdTest extends BasePipelineTest {
         def result = getIdFromArtifactId(artifactId: '(koji-build:46436038,koji-build:46436038)->fedora-update:FEDORA-2020-008cb761a2')
         assertEquals '46436038,46436038', result
     }
+
+    @Test
+    void testCompositeArtifactRH() {
+        def result = getIdFromArtifactId(artifactId: '(brew-build:33168992)->dist-git-pr:60ae9e6ccd224b82b1c9052d6304d5bb@hash#0')
+        assertEquals '33168992', result
+    }
 }
