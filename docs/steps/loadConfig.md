9b1a5d285ef33a2b7eaad42a0efaa655509a7f0d
Michal Srb <michal@redhat.com>
2021-03-12 13:41:51 +0100

    [loadConfig] Make the profile param optional

--- docs/steps/loadConfig.md
+++ docs/steps/loadConfig.md
@@ -1,16 +1,17 @@
 # loadConfig() step
 
-This step loads test profile configuration from the pipeline config file.
+This step loads the pipeline configuration from a config file.
+Optionally, if the profile name is specified, only the profile configuration is returned.
 The config file needs to be in the same repository with the pipeline definition (Jenkinsfile).
 The default name of the config file is `config.json`. 
 
 
 ## Parameters
 
-* **profile**: string; name of the test profile to load
+* **profile**: string; (optional) name of the test profile to load
 
 ## Example Usage
 
 ```groovy
-loadConfig(profile: 'f34')
+loadConfig(profile: 'f35')
 ```
--- vars/loadConfig.groovy
+++ vars/loadConfig.groovy
@@ -8,20 +8,19 @@ def call(Map params = [:]) {
     def profileName = params.get('profile')
     def configFile = 'config.json'
 
-    if (!profileName) {
-        error('No profile name specified, cannot continue...')
-    }
-
     if (!fileExists(configFile)) {
         error("Config file \"${configFile}\" doesn't exist")
     }
 
     def config = readJSON(file: configFile)
-    def profile = config.get('profiles', [:])?.get(profileName)
 
-    if (profile == null) {
-        error("Profile \"${profileName}\" doesn't exist in ${configFile}")
+    if (profileName) {
+        def profile = config.get('profiles', [:])?.get(profileName)
+        if (profile == null) {
+            error("Profile \"${profileName}\" doesn't exist in ${configFile}")
+        }
+        return profile
     }
 
-    return profile
+    return config
 }
