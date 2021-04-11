9de7a6415a9dd43a8ae9384fcae71d8d55941a2d
Michal Srb <michal@redhat.com>
2021-04-11 10:36:12 +0200

    [waitForTestingFarmResults] Wait in a shell script

--- vars/waitForTestingFarmResults.groovy
+++ vars/waitForTestingFarmResults.groovy
@@ -1,8 +1,6 @@
 #!/usr/bin/groovy
 
 import groovy.json.JsonSlurperClassic
-import groovy.time.TimeDuration
-import groovy.time.TimeCategory
 
 
 /**
@@ -21,65 +19,39 @@ def call(Map params = [:]) {
 
     apiUrl = apiUrl + '/v0.1/requests/' + "${requestId}"
 
-    def wait = true
-    def response
-    def state
-    def napTime
-    def notFirstCheck = false
-
-    def timeStart = new Date()
-    def timeNow
-
     // FIXME: this is here for easier debugging in the early stages; let's remove it once
     // things are more stable
     def tfArtifactsBaseUrl = env.FEDORA_CI_PAGURE_DIST_GIT_URL.startsWith('https://src.osci') ? "http://artifacts.osci.redhat.com/testing-farm" : "http://artifacts.dev.testing-farm.io"
 
-    while (wait) {
-        retry(30) {
-            try {
-                response = httpGet(apiUrl)
-            } catch(e) {
-                // TODO: remove
-                echo "Testing Farm Artifacts URL: ${tfArtifactsBaseUrl}/${requestId}"
+    echo "Testing Farm API Request URL: ${apiUrl}"
+    echo "Testing Farm Artifacts URL: ${tfArtifactsBaseUrl}/${requestId}"
+    echo "\n"
 
-                echo "ERROR: Oops, something went wrong. We were unable to call ${apiUrl} — let's wait 120 seconds and then try again: ${e.getMessage()}"
-                sleep(time: 120, unit: "SECONDS")
-                error("Failed to call Testing Farm: ${e.getClass().getCanonicalName()}: ${e.getMessage()}")
-            }
-        }
-        // TODO: remove
-        echo "Testing Farm API Request URL: ${apiUrl}"
-        echo "Testing Farm Artifacts URL: ${tfArtifactsBaseUrl}/${requestId}"
-        state = response.get('state')
-        if (state in ['complete', 'error']) {
-            return response
-        }
-        napTime = 90
-        if (state in ['new', 'queued'] && notFirstCheck) {
-            // the request can stay queued for a very long time;
-            // if that is the case, don't check the status that often
-            napTime = 600
-        }
+    sh(
+        script: """
+set +x
+while true
+do
+  state="\$(curl --retry 10 --retry-connrefused --connect-timeout 10 --retry-delay 30  -s '${apiUrl}' | jq --raw-output ''.state'')"
+  if [ "\$state" = "complete" ] || [ "\$state" = "error" ]; then
+    echo "Done! The current status \\"\$state\\"."
+    break
+  fi
+  echo "The current status is \\"\$state\\"."
+  echo "Waiting for Testing Farm..."
+  sleep 120
+done
+""", label: "Wait for test results"
+    )
 
-        checkTimeout(timeStart, timeout)
-
-        sleep(time: napTime, unit: "SECONDS")
-        notFirstCheck = true
-    }
-}
-
-@NonCPS
-def checkTimeout(timeStart, timeout) {
-    timeNow = new Date()
-    TimeDuration duration = TimeCategory.minus(timeNow, timeStart)
-    if (duration.getMinutes() >= timeout) {
-        error("Timeout reached and there are still no test results")
-    }
-}
-
-
-def httpGet(url) {
-    def response = httpRequest consoleLogResponseBody: false, contentType: 'APPLICATION_JSON', httpMode: 'GET', url: "${url}", validResponseCodes: '200', quiet: true
+    def response = httpRequest(
+        consoleLogResponseBody: false,
+        contentType: 'APPLICATION_JSON',
+        httpMode: 'GET',
+        url: "${apiUrl}",
+        validResponseCodes: '200',
+        quiet: true
+    )
     def contentJson = new JsonSlurperClassic().parseText(response.content)
     return contentJson
 }
