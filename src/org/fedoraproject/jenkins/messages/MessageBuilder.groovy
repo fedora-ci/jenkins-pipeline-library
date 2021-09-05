7384a7eeff74e01db6ce83c93b2442208eda62cb
Michal Srb <michal@redhat.com>
2021-09-05 09:32:32 +0200

    Go back to 0.2.1 message version
    Services are not ready yet for such a big, backward-compatible leap yet
    :P

--- src/org/fedoraproject/jenkins/messages/MessageBuilder.groovy
+++ src/org/fedoraproject/jenkins/messages/MessageBuilder.groovy
@@ -11,7 +11,7 @@ import org.fedoraproject.jenkins.messages.FedoraUpdateMessageBuilder
 
 
 def getMessageVersion() {
-    return '1.1.6'
+    return '0.2.1'
 }
 
 def getPipelineSection(artifactType, taskId, pipelineMetadata) {
