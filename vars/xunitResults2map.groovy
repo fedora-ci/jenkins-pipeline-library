dd9d4bb3777148715b387e5cfdb4d1769c557153
Michal Srb <michal@redhat.com>
2021-09-06 08:36:03 +0200

    Fix function name
    It's "xunitResults2map", not "xunit2simpleMap".

--- vars/xunitResults2map.groovy
+++ vars/xunitResults2map.groovy
@@ -9,5 +9,5 @@ import org.fedoraproject.jenkins.Utils
 def call(Map params = [:]) {
     def xunit = params.get('xunit')
 
-    return Utils.xunit2simpleMap(xunit)
+    return Utils.xunitResults2map(xunit)
 }
