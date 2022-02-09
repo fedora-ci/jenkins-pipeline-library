a57359d359c1fb16994d66229d9907e46b69ab8c
Michal Srb <michal@redhat.com>
2022-02-09 12:59:53 +0100

    [evaluateTestingFarmResults] Recognize when testing was skipped

--- vars/evaluateTestingFarmResults.groovy
+++ vars/evaluateTestingFarmResults.groovy
@@ -14,32 +14,32 @@ def call(result) {
         return
     }
 
+    def message = ''
+    if (result.get('result', [:])?.get('summary')) {
+        message = result['result']['summary']
+    } else if (result.get('notes')) {
+        // the whole result.result section may be missing if TF failed to dispatch the request;
+        // but we can still check if there are any message with explanation in the notes...
+        result.get('notes').each { note ->
+            if (note.get('level') == 'error') {
+                // this should be the note which describes what happened
+                message = note.get('message') ?: message
+            }
+        }
+    }
+    if (message) {
+        // we can store the message in the environment
+        // and then reuse it for example when sending messages
+        // to the message bus
+        env.ERROR_MESSAGE = message
+    }
+
     // if the result is "error", we stop the pipeline and set the Jenkins build status to "FAILURE" (default for errors)
     catchError {
         if (!result || result.get('state') == 'error' || result.get('result')?.get('overall') == 'error') {
-            def message = ''
-            if (result.get('result', [:])?.get('summary')) {
-                message = result['result']['summary']
-            } else if (result.get('notes')) {
-                // the whole result.result section may be missing if TF failed to dispatch the request;
-                // but we can still check if there are any message with explanation in the notes...
-                result.get('notes').each { note ->
-                    if (note.get('level') == 'error') {
-                        // this should be the note which describes what happened
-                        message = note.get('message') ?: message
-                    }
-                }
-            }
-
-            if (message) {
-                // error() call down below prints the message to the log,
-                // but we can store it in an environment variable as well
-                // and then reuse it for example when sending messages
-                // to the message bus
-                env.ERROR_MESSAGE = message
-            } else {
+            if (!message) {
                 // we don't know what happened...
-                message = 'Infrastructure Error :/'
+                message = 'Infrastructure Error'
             }
             error(message)
         }
@@ -48,7 +48,10 @@ def call(result) {
     // if the result is "failed", we stop the pipeline and set the Jenkins build status to "UNSTABLE"
     catchError(buildResult: 'UNSTABLE') {
         if (result.get('state') == 'complete' && result.get('result')?.get('overall') == 'failed') {
-            error('There are test failures.')
+            error("There are test failures. ${message}")
+        }
+        if (result.get('state') == 'complete' && result.get('result')?.get('overall') == 'skipped') {
+            error("Tests were skipped. ${message}")
         }
     }
 }
