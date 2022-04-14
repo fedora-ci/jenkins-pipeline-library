5807e8f36087caa1e4d9bbe92d582a89457da79a
Michal Srb <michal@redhat.com>
2022-04-14 12:11:06 +0200

    [sendMessage] Drop contact.url field
    We don't have CI docs.

--- src/org/fedoraproject/jenkins/messages/ModuleMessageBuilder.groovy
+++ src/org/fedoraproject/jenkins/messages/ModuleMessageBuilder.groovy
@@ -23,7 +23,6 @@ def buildMessageQueued(
     // contact section
     msgTemplate['contact']['name'] = pipelineMetadata['pipelineName']
     msgTemplate['contact']['team'] = pipelineMetadata['maintainer']
-    msgTemplate['contact']['url'] = pipelineMetadata['docs']
     msgTemplate['contact']['docs'] = pipelineMetadata['docs']
     msgTemplate['contact']['irc'] = pipelineMetadata['contact']['irc']
     msgTemplate['contact']['email'] = pipelineMetadata['contact']['email']
@@ -83,7 +82,6 @@ def buildMessageRunning(
     // contact section
     msgTemplate['contact']['name'] = pipelineMetadata['pipelineName']
     msgTemplate['contact']['team'] = pipelineMetadata['maintainer']
-    msgTemplate['contact']['url'] = pipelineMetadata['docs']
     msgTemplate['contact']['docs'] = pipelineMetadata['docs']
     msgTemplate['contact']['irc'] = pipelineMetadata['contact']['irc']
     msgTemplate['contact']['email'] = pipelineMetadata['contact']['email']
@@ -146,7 +144,6 @@ def buildMessageComplete(
     // contact section
     msgTemplate['contact']['name'] = pipelineMetadata['pipelineName']
     msgTemplate['contact']['team'] = pipelineMetadata['maintainer']
-    msgTemplate['contact']['url'] = pipelineMetadata['docs']
     msgTemplate['contact']['docs'] = pipelineMetadata['docs']
     msgTemplate['contact']['irc'] = pipelineMetadata['contact']['irc']
     msgTemplate['contact']['email'] = pipelineMetadata['contact']['email']
@@ -228,7 +225,6 @@ def buildMessageError(
     // contact section
     msgTemplate['contact']['name'] = pipelineMetadata['pipelineName']
     msgTemplate['contact']['team'] = pipelineMetadata['maintainer']
-    msgTemplate['contact']['url'] = pipelineMetadata['docs']
     msgTemplate['contact']['docs'] = pipelineMetadata['docs']
     msgTemplate['contact']['irc'] = pipelineMetadata['contact']['irc']
     msgTemplate['contact']['email'] = pipelineMetadata['contact']['email']
