b4962c3d6848216c6c1603d9d65104d18988ff51
Michal Srb <michal@redhat.com>
2023-02-18 18:11:31 +0100

    Add getTaskInfo() step
    Signed-off-by: Michal Srb <michal@redhat.com>

---
+++ docs/steps/getTaskInfo.md
@@ -0,0 +1,13 @@
+# getTaskInfo() step
+
+Return information about given Koji task.
+
+## Parameters
+
+* **taskId**: integer; Koji task ID
+
+## Example Usage
+
+```groovy
+def taskInfo = getTaskInfo(93788373)
+```
---
+++ vars/getTaskInfo.groovy
@@ -0,0 +1,28 @@
+#!/usr/bin/groovy
+
+import org.fedoraproject.jenkins.koji.Koji
+
+
+/**
+ * getTaskInfo() step.
+ */
+def call(taskId) {
+    def koji = new Koji(env.KOJI_API_URL)
+    def taskInfo = koji.getTaskInfo(taskId.toInteger())
+
+    // TODO: we can do this mapping directly in TaskInfo class
+    def taskInfoMap = [
+        'id': taskInfo.id,
+        'method': taskInfo.method,
+        'ownerId': taskInfo.ownerId,
+        'target': taskInfo.target,
+        'scratch': taskInfo.scratch,
+        'name': taskInfo.name,
+        'packageName': taskInfo.packageName,
+        'version': taskInfo.version,
+        'release': taskInfo.release,
+        'nvr': taskInfo.nvr
+    ]
+
+    return taskInfoMap
+}
