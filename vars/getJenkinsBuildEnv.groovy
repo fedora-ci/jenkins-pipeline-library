270b16784c54144dd988b7bcaba7257cc84c1614
Michal Srb <michal@redhat.com>
2025-03-30 13:40:18 +0200

    Add getJenkinsBuildEnv() step
    Signed-off-by: Michal Srb <michal@redhat.com>

---
+++ vars/getJenkinsBuildEnv.groovy
@@ -0,0 +1,27 @@
+#!/usr/bin/groovy
+
+
+/**
+ * getJenkinsBuildEnv() step.
+ */
+def call(Map params = [:]) {
+
+    def jobName = params.get('jobName')
+    def buildNumber = params.get('buildNumber')
+
+    def job = Jenkins.instance.getItemByFullName(jobName)
+    if (!job) {
+        error("Job \"${jobName}\" doesn't exist")
+    }
+
+    if (buildNumber == null) {
+        error("Missing required parameter: buildNumber")
+    }
+
+    def build = job.getBuildByNumber(buildNumber)
+    if (!build) {
+        error("Build #${buildNumber} for job \"${jobName}\" does not exist")
+    }
+
+    return new RunWrapper(build, true).buildVariables
+}
